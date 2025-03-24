347fbeabf9253962803dbb46171d450b39624cb0
Daily Best News App <146649776+ridoy520@users.noreply.github.com>
2025-03-24 15:27:24 +0600

    Update background-changer.js color function comment

--- background-changer.js
+++ background-changer.js
@@ -1,13 +1,19 @@
 // Predefined background image URLs
 const backgroundImages = [
-    'https://source.unsplash.com/random/1920x1080/?nature',
-    'https://source.unsplash.com/random/1920x1080/?landscape',
-    'https://source.unsplash.com/random/1920x1080/?mountain',
-    'https://source.unsplash.com/random/1920x1080/?ocean',
-    'https://source.unsplash.com/random/1920x1080/?forest'
+    'https://images.pexels.com/photos/2486168/pexels-photo-2486168.jpeg',
+    'https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTExL3Jhd3BpeGVsb2ZmaWNlMTBfZmx1aWRfYWJzdHJhY3Rpb25fYmFja2dyb3VuZF9faGludF9vZl9wcmVjaXNpb18wMjk5YmM1ZS0yNjAyLTQxODgtOTExMy1kZTVkZTk1YWVmN2FfMS5qcGc.jpg',
+    'https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjEwMTMtcC0wMDE5ZC0wMS1rc2k4YjVqbi5qcGc.jpg',
+    'https://www.skyweaver.net/images/media/wallpapers/wallpaper1.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlfxps-j1QZ0F36RZ8PnlC9FCEe5npD_fX7qKLk6u1ICB1YCJkJz2fr4JWi6ghgt-TVYY&usqp=CAU',
+    'https://e0.pxfuel.com/wallpapers/43/473/desktop-wallpaper-anime-nature.jpg',
+    'https://wallpapercrafter.com/th800/411414-Anime-Landscape-Phone-Wallpaper.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvg77xG3EqkXpWX3XJztSvTlzY9OXg4XkwUw&s',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZSf3OywU4xlS9bYZLxRh2SLOoBPwaMDI5gA&s',
+    'https://applescoop.org/image/wallpapers/iphone/iphone-16-teal-21-09-2024-1726985415-hd-wallpaper.png',
+    'https://i.pinimg.com/736x/a3/a7/51/a3a7511380de5ac4e1d5bb3c0d278146.jpg'
 ];
 
-// Predefined gradient colors
+/*// Predefined gradient colors
 const gradientColors = [
     ['#2193b0', '#6dd5ed'],
     ['#cc2b5e', '#753a88'],
@@ -15,7 +21,7 @@ const gradientColors = [
     ['#16a085', '#f4d03f'],
     ['#603813', '#b29f94']
 ];
-
+*/
 // Function to get random item from array
 function getRandomItem(array) {
     return array[Math.floor(Math.random() * array.length)];
@@ -24,7 +30,7 @@ function getRandomItem(array) {
 // Function to update background
 function updateBackground() {
     const imageUrl = getRandomItem(backgroundImages);
-    const colors = getRandomItem(gradientColors);
+    //const colors = getRandomItem(gradientColors);
     const angle = Math.floor(Math.random() * 360);
 
     document.body.style.backgroundImage = `
